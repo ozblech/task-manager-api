0e85f17700c68ce5473cf5b06d8ffc832ae139b0
ozblech <ozblech87@gmail.com>
2020-04-15 14:46:30 +0300

    fix

--- src/routers/user.js
+++ src/routers/user.js
@@ -31,7 +31,7 @@ router.post('/users' , async (req, res) => {
 })
 
 //login
-router.post('/users/login   ', async (req, res) => {
+router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
