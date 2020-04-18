1ea95700711e79bb82e6dd36a6a5414a7262d213
ozblech <ozblech87@gmail.com>
2020-04-18 15:25:11 +0300

    fix

--- src/emails/account.js
+++ src/emails/account.js
@@ -8,7 +8,7 @@ const sendWelcomeEmail = (email, name) => {
         to: email,
         from: '7391987@gmail.com',
         subject: 'Thanks for joining in',
-        text: `Welcome to the app, ${name}. Let me know hou you get along with the app.`
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app. Oz Blech`
     })
 }
 
