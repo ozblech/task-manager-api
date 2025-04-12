3d9d55b804db7bd70a8d2636c1c10c161297c19f
Oz Blech <ozblech87@gmail.com>
2025-04-12 11:51:20 +0300

    Added kubernetes secret

--- src/emails/account.js
+++ src/emails/account.js
@@ -6,7 +6,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: '7391987@gmail.com',
+        from: process.env.EMAIL_ACCOUNT,
         subject: 'Thanks for joining in',
         text: `Welcome to the app, ${name}. Let me know how you get along with the app. Oz Blech`
     })
@@ -15,7 +15,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancelEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: '7391987@gmail.com',
+        from: process.env.EMAIL_ACCOUNT,
         subject: 'Task-App cancelled',
         text: `Hello ${name}. We are sorry to hear that you leave us. Please tell us if there is something we could do better for you.`
     })
